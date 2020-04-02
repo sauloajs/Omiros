aab9a8ff81c912a3e5ba0b88f3d2206f92956166
Saulo Silva <39671086+sauloajs@users.noreply.github.com>
2020-04-02 15:31:23 -0300

    And now finnaly the correct smtp credentials

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,7 @@ return [
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => 'Omiros',
+            'username' => 'contatoomiros@gmail.com',
             'password' => 'saulosilva',
         ],
 
