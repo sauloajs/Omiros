c83287ba593c7960c1b65fa8a6f32e51f09a850c
Saulo Silva <39671086+sauloajs@users.noreply.github.com>
2020-04-02 09:28:06 -0300

    Add configuration to send email

--- config/mail.php
+++ config/mail.php
@@ -39,8 +39,8 @@ return [
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => 'Omiros',
+            'password' => 'saulosilva',
         ],
 
         'ses' => [
@@ -74,7 +74,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'silvasaulo471@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'contatoomiros@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Omiros'),
     ],
 
